fd5d781077b22683ae04bd8ad79fd1741798e61e
Luis Cruz <luismirandacruz@gmail.com>
2018-03-06 01:10:23 +0000

    added number of occurences

--- docs/energy_patterns/main.js
+++ docs/energy_patterns/main.js
@@ -3,9 +3,35 @@
     $(document).ready(function () {
         $.get('./patterns.yml')
         .done(function (data) {
-          console.log('File load complete');
           var patterns = jsyaml.load(data);
-          console.log(patterns);
+
+          Handlebars.registerHelper("total_occurrences", function(pattern) {
+            var sum = 0;
+            if(pattern.occurrences_android){
+              if(pattern.occurrences_android.issues){
+                sum += pattern.occurrences_android.issues.length
+              }
+              if(pattern.occurrences_android.commits){
+                sum += pattern.occurrences_android.commits.length
+              }
+              if(pattern.occurrences_android.pull_requests){
+                sum += pattern.occurrences_android.pull_requests.length
+              }
+            }
+            if(pattern.occurrences_ios){
+              if(pattern.occurrences_ios.issues){
+                sum += pattern.occurrences_ios.issues.length
+              }
+              if(pattern.occurrences_ios.commits){
+                sum += pattern.occurrences_ios.commits.length
+              }
+              if(pattern.occurrences_ios.pull_requests){
+                sum += pattern.occurrences_ios.pull_requests.length
+              }
+            }
+            return sum;
+          });
+
           var source   = document.getElementById("pattern-template").innerHTML;
           var template = Handlebars.compile(source);
           var html = template(patterns);
