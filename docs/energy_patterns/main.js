0f281d457d8a23213ceadb00faf5f63b6c99d0d6
Luis Cruz <ei06034@fe.up.pt>
2018-04-20 17:50:24 +0100

    pattern show

--- docs/energy_patterns/main.js
+++ docs/energy_patterns/main.js
@@ -32,10 +32,37 @@
             return sum;
           });
 
-          var source   = document.getElementById("pattern-template").innerHTML;
-          var template = Handlebars.compile(source);
-          var html = template(patterns);
-          $("#patterns-list-placeholder").html(html);
-      });
+          Handlebars.registerHelper("list_occurrences", function(occurrences_list) {
+            console.log(occurrences_list)
+            var html = $('<ul></ul>').addClass('list-group');
+            occurrences_list.forEach(function(element){
+              html.append($("<li><a href='"+element+"'>"+element+"</a></li>").addClass('list-group-item'));
+            });
+            return html.html();
+          });
+
+          var listPatterns = function () {
+            var source   = document.getElementById("patterns-list-template").innerHTML;
+            var template = Handlebars.compile(source);
+            var html = template(patterns);
+            $("#patterns-list-placeholder").html(html);
+          }
+
+          var viewPattern = function (name) {
+            var patternData = patterns.find(x => x['name'] == unescape(name))
+            var source   = document.getElementById("pattern-show-template").innerHTML;
+            var template = Handlebars.compile(source);
+            var html = template(patternData);
+            $("#pattern-show-placeholder").html(html);
+          };
+
+          var routes = {
+            '/patterns/': listPatterns,
+            '/patterns/:name': viewPattern
+          };
+
+          var router = Router(routes).configure({'notfound':listPatterns});
+          router.init();
+        });
     });
 }());
