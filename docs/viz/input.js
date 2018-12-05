b6d2856b95bd835412a95b8af06140b4bc304c4d
Rui Maranhao <rui@computer.org>
2018-12-05 16:20:31 +0000

    make it more color-blind friendly
    Source: https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/

--- docs/viz/input.js
+++ docs/viz/input.js
@@ -30,15 +30,9 @@ const array = ['Dark UI Colors', 'Dynamic Retry Delay', 'Avoid Extraneous Work',
   'Enough resolution', 'Sensor Fusion', 'Kill Abnormal Tasks',
   'No screen Interaction', 'Avoid Extra. Graph. & Anim.', 'Manual Sync - On Demand']
 
-const colorArray = [
-  '#034e7b','#feb24c','#b10026','#238443','#fdbb84','#ffffb2','#fed976',
-  '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6',
-  '#999966', '#99FF99', '#B34D4D', '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A',
-  '#33FFCC', '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', '#66664D',
-  '#991AFF', '#E666FF', '#4DB3FF', '#1AB399', '#E666B3', '#33991A', '#CC9999',
-  '#B3B31A', '#00E680', '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', '#E64D66', '#4DB380',
-  '#FF4D4D', '#99E6E6', '#6666FF'];
+const colorArray = ['#e6194B', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4',
+  '#42d4f4', '#f032e6', '#bfef45', '#fabebe', '#469990', '#e6beff', '#9A6324', '#fffac8',
+  '#800000', '#aaffc3', '#808000', '#ffd8b1', '#000075', '#a9a9a9', '#ffffff', '#000000']
 
 const originalColor = ['#034e7b','#feb24c','#b10026','#238443','#fdbb84',
   '#ffffb2','#fed976'];
