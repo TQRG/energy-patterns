c5cf487d13c1fa6cc381eb3c78172fbe3d7daca3
Luis Cruz <luismirandacruz@gmail.com>
2026-02-22 20:36:06 +0100

    migrate website to Jekyll with GitHub Actions deployment

---
+++ docs/_plugins/pattern_pages.rb
@@ -0,0 +1,31 @@
+module Jekyll
+  class PatternPageGenerator < Generator
+    safe true
+
+    def generate(site)
+      patterns = site.data["patterns"]
+      return unless patterns
+
+      patterns.each do |pattern|
+        next unless pattern["name"]
+        site.pages << PatternPage.new(site, site.source, pattern)
+      end
+    end
+  end
+
+  class PatternPage < Page
+    def initialize(site, base, pattern)
+      @site = site
+      @base = base
+      @dir  = "patterns"
+      @name = Jekyll::Utils.slugify(pattern["name"]) + ".html"
+
+      process(@name)
+      read_yaml(File.join(base, "_layouts"), "pattern.html")
+
+      data["pattern"]   = pattern
+      data["title"]     = pattern["name"]
+      data["permalink"] = "/patterns/#{Jekyll::Utils.slugify(pattern["name"])}/"
+    end
+  end
+end
